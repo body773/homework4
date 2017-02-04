d39992b05d0ac9076597ef06bcd8d339a6df4184
Bohdan <body.nv@gmail.com>
2017-02-04 09:01:21 +0000

    new files

---
+++ js/scrollto.js
@@ -0,0 +1,16 @@
+
+
+$("#sidebar > a").click(function(e) { 
+          // Prevent a page reload when a link is pressed
+        e.preventDefault(); 
+          // Call the scroll function
+        goToByScroll($(this).attr("id"));           
+    });
+function goToByScroll(id){
+          // Reove "link" from the ID
+        id = id.replace("link", "");
+          // Scroll
+        $('html,body').animate({
+            scrollTop: $("#"+id).offset().top - 50},
+            'slow');
+}
